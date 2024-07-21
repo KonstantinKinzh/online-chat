83f9ad51df5ddf7da46eb68bf685a4b8d4381a11
KonstantinKinzh <kinzhitayev95@mail.ru>
2024-07-21 19:05:54 +0300

    added firebase initialization

---
+++ project-files/src/firebase/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIGDJnmaA9oGikLk6xKXrSht4oAJZrSo4",
+  authDomain: "online-chat-cb5fd.firebaseapp.com",
+  projectId: "online-chat-cb5fd",
+  storageBucket: "online-chat-cb5fd.appspot.com",
+  messagingSenderId: "394679962565",
+  appId: "1:394679962565:web:ff2a05a187387813dad9ac"
+};
+
+const app = initializeApp(firebaseConfig);
--- project-files/src/main.tsx
+++ project-files/src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
+import "@/firebase/firebase";
 import './main.css';
 
 const rootId = document.getElementById("root");
